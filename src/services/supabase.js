b96b7caeb12fe2379047b20e90cc1553d3c15730
Paulo Amorim <phdja25@gmail.com>
2023-10-04 19:25:19 -0300

    set Supabase up.

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://wfjeuwqovaksboflcwfj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmamV1d3FvdmFrc2JvZmxjd2ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY0MzUwMTQsImV4cCI6MjAxMjAxMTAxNH0.WNOlKZOO1vXZ6X7CXQ4ec50fz156NEbvkKLSvnUnQm8";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
